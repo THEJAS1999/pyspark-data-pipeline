571db57709f1b79afb1d3cdbe0b3d39e31070905
Thejas <thejasthejas30@gmail.com>
2026-04-01 11:24:29 +0000

    Update file paths in pipeline runner for bronze, silver, and gold layers

--- src/pipeline_runner.py
+++ src/pipeline_runner.py
@@ -1,10 +1,10 @@
 # Pipeline Runner
 
 # Bronze
-exec(open("/Workspace/Repos/bronze.py").read())
+exec(open("/Workspace/Users/thejasthejas30@gmail.com/pyspark-data-pipeline/src/bronze.py").read())
 
 # Silver
-exec(open("/Workspace/Repos/silver.py").read())
+exec(open("/Workspace/Users/thejasthejas30@gmail.com/pyspark-data-pipeline/src/silver.py").read())
 
 # Gold
-exec(open("/Workspace/Repos/gold.py").read())
+exec(open("/Workspace/Users/thejasthejas30@gmail.com/pyspark-data-pipeline/src/gold.py").read())
